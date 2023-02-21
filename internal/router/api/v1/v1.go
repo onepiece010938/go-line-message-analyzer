f1214e9631375bda44341f0fc6b833121f50761e
onepiece010938 <onepiece010938@gmail.com>
2023-02-21 13:25:50 +0800

    ✨ feat(callback): add callback func
    add callback function for linebot webhook

--- internal/router/api/v1/v1.go
+++ internal/router/api/v1/v1.go
@@ -9,6 +9,7 @@ import (
 func RegisterRouter(router *gin.RouterGroup, app *app.Application) {
 	v1 := router.Group("/v1")
 	{
+		v1.POST("/callback", Callback(app))
 		v1.GET("/sample", SAMPLE)
 		v1.GET("/analyze", StartAnalyze(app))
 	}
